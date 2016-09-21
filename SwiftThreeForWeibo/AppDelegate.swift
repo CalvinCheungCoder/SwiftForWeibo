846aebc573299c0972f7aa7236f1acda12b2ab00
zhangdinghao <zhangdinghao@zhangdinghaodeMacBook-Pro.local>
2016-09-21 18:56:20 +0800

    基本设置

---
+++ SwiftThreeForWeibo/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  SwiftThreeForWeibo
+//
+//  Created by 张丁豪 on 16/9/21.
+//  Copyright © 2016年 张丁豪. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = WBMainViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
