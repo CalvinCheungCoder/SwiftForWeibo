57b158f5bdc0aba7ad5c21d2b9294baff68c10da
zhangdinghao <zhangdinghao@zhangdinghaodeMacBook-Pro.local>
2016-09-21 19:29:50 +0800

    测试扩展控制器

--- SwiftThreeForWeibo/Class/View/Main/WBDemoViewController.swift
+++ SwiftThreeForWeibo/Class/View/Main/WBDemoViewController.swift
@@ -13,23 +13,27 @@ class WBDemoViewController: WBBaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 设置标题
+        title = "第 \(navigationController?.childViewControllers.count ?? 0) 个"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func showNext() {
+        
+        let vc = WBDemoViewController()
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension WBDemoViewController {
+    
+    override func MakeUI() {
+    
+        super.MakeUI()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "下一个", style: .plain, target: self, action: #selector(showNext))
     }
-    */
-
 }
